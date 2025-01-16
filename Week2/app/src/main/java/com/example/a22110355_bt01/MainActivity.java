157e49b1b32d0a0b13f5d529d037f9b4ae6e3364
mkhoavo <v.minhkhoa123456@gmail.com>
2025-01-16 14:18:53 +0700

    bt4: lod in android studio

--- Week2/app/src/main/java/com/example/a22110355_bt01/MainActivity.java
+++ Week2/app/src/main/java/com/example/a22110355_bt01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a22110355_bt01;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);//intflag, int mask
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(8);
+        numbers.add(11);
+        numbers.add(13);
+        numbers.add(15);
+
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                Log.d("SoNT", "Số nguyên tố: " + number);
+            }
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -26,5 +45,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
